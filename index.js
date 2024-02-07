60c82fa999e367a9eb4a8be3110cc65d6e696027
ZachMcD223 <148635518+ZachMcD223@users.noreply.github.com>
2024-02-06 20:10:59 -0600

    index.js

--- index.js
+++ index.js
@@ -2,10 +2,50 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
+function handleDirectionChange(direction){
+
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
+}
+
+
+let direction = null;
+let x = 100;
+let y = 250;
+function moveCharacter(){
+if(direction === 'west'){x=x-1}
+if(direction === 'north'){y=y+1}
+if(direction === 'east'){x=x+1}
+if(direction === 'south'){y=y-1}
+}
+character.style.left = x+'px'
+character.style.bottom = y+'px'
 move(character).to(100, 250)
+setInterval(function(){
+    if(direction === 'west'){x=x-1}
+    if(direction === 'north'){y=y+1}
+    if(direction === 'east'){x=x+1}
+    if(direction === 'south'){y=y-1}
+    },1)
+setInterval(moveCharacter, 1)
 
-
-move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
@@ -13,3 +53,36 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+document.addEventListener('keydown', function(e){ 
+    if(e.repeat) return;               
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
